88f5b113f43609b1a354f5d90158abde9eca5c45
Zhihang Fan <Canoe@Zhihangs-MacBook-Air.local>
2014-07-06 16:16:13 +0800

    简单题目；关键是固定一个位置，简化问题

---
+++ SRM613/TaroFriends.cpp
@@ -0,0 +1,87 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <string>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 1 << 29;
+
+class TaroFriends
+{
+    public:
+        vector <int> C;
+        int n, X;
+        int minDistance(int leftMost, int index) {
+            int rightMost = -INF;
+            for (int i = 0; i < n; i++) {
+                if (i == index) {
+                    continue;
+                }
+                if (C[i] - X >= leftMost && C[i] + X >= leftMost) {
+                    rightMost = max(rightMost, C[i] - X);
+                } else if (C[i] + X >= leftMost) {
+                    rightMost = max(rightMost, C[i] + X);
+                }
+                else {
+                    rightMost = -INF;
+                    break;
+                }
+            }
+            int ret = INF;
+            if (rightMost != -INF) {
+                ret = rightMost - leftMost;
+            }
+            return ret;
+        }
+        int getNumber(vector <int> coordinates, int X) {
+            C = coordinates;
+            this->X = X;
+            n = C.size();
+            if (n == 1) {
+                return 0;
+            }
+            int ret = INF;
+            for (int i = 0; i < n; i++) {
+                ret = min(minDistance(C[i] - X, i), ret);
+                ret = min(minDistance(C[i] + X, i), ret);
+            }
+            return ret;
+        }
+        
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {-3, 0, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 3; verify_case(0, Arg2, getNumber(Arg0, Arg1)); }
+	void test_case_1() { int Arr0[] = {4, 7, -7}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 5; int Arg2 = 4; verify_case(1, Arg2, getNumber(Arg0, Arg1)); }
+	void test_case_2() { int Arr0[] = {-100000000, 100000000}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 100000000; int Arg2 = 0; verify_case(2, Arg2, getNumber(Arg0, Arg1)); }
+	void test_case_3() { int Arr0[] = {3, 7, 4, 6, -10, 7, 10, 9, -5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 7; int Arg2 = 7; verify_case(3, Arg2, getNumber(Arg0, Arg1)); }
+	void test_case_4() { int Arr0[] = {-4, 0, 4, 0}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; int Arg2 = 4; verify_case(4, Arg2, getNumber(Arg0, Arg1)); }
+	void test_case_5() { int Arr0[] = {7}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; int Arg2 = 0; verify_case(5, Arg2, getNumber(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+
+int main(){
+    TaroFriends ___test;
+    ___test.run_test(-1);
+    return 0;
+}
+// END CUT HERE
